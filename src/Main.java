eb4c4e73181b8ee000ec260c5d4ae4a85d6110f5
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-09-11 22:41:54 +0300

    Домашка3.Переменные1.Задача7.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        var boxerOne = 78.2;
+        System.out.println(boxerOne);
+        var boxerTwo = 82.7;
+        System.out.println(boxerTwo);
+        var difference = boxerTwo - boxerOne;
+        System.out.println(difference);
+        var differenceDivision = boxerTwo % boxerOne;
+        System.out.println(differenceDivision);
+    }
+}
